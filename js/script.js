08f1ebe48b1b69b0038aa9907a7f1acae57d75be
Alistair Saldanha <shaanalistair47@gmail.com>
2022-07-07 08:41:51 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function changecontent1() {
+    document.getElementById("img1").src = "/assets/images/training-image-01.jpg"
+    document.getElementById("h1").innerHTML = "First Training Class"
+}
+function changecontent2() {
+    document.getElementById("img1").src = "/assets/images/training-image-02.jpg"
+    document.getElementById("h1").innerHTML = "Second Training Class"
+}
+function changecontent3() {
+    document.getElementById("img1").src = "/assets/images/training-image-03.jpg"
+    document.getElementById("h1").innerHTML = "Third Training Class"
+}
+function changecontent4() {
+    document.getElementById("img1").src = "/assets/images/training-image-04.jpg"
+    document.getElementById("h1").innerHTML = "Fourth Training Class"
+}
